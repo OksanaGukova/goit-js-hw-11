a9ac05b2bd9b03463f585f2bd1991ffecfb465e5
OksanaGukova <152022817+OksanaGukova@users.noreply.github.com>
2024-05-07 19:14:05 +0000

    Deploying to gh-pages from @ OksanaGukova/goit-js-hw-11@1973578996807bae2e10212a2c308bb01fa0d3c6 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,23 @@
-import{S as s}from"./assets/vendor-5af972a3.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))c(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&c(o)}).observe(document,{childList:!0,subtree:!0});function i(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function c(e){if(e.ep)return;e.ep=!0;const r=i(e);fetch(e.href,r)}})();document.querySelector(".img-container");const l=document.querySelector(".search-form");document.querySelector(".loader");l.addEventListener("submit",u);function u(n){n.preventDefault()}new s(".gallery a",{captionDelay:250});
+import{i as f,S as m}from"./assets/vendor-8c59ed88.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();function p({tags:o,webformatURL:r,largeImageURL:s,likes:i,views:e,comments:t,downloads:n}){const u=`
+    <div class="photo-list">
+      <a href='${s}' class="list">
+        <img class="photo" src="${r}" alt="${o}" loading="lazy" />
+      </a>
+      <div class="info">
+        <ul>
+          <li class="info-item">
+            <p>Likes: ${i}</p>
+          </li>
+          <li class="info-item">
+            <p>Views: ${e}</p>
+          </li>
+          <li class="info-item">
+            <p>Comments: ${t}</p>
+          </li>
+          <li class="info-item">
+            <p><b>Downloads:</b> ${n}</p>
+          </li>
+        </ul>
+      </div>
+    </div>`;a.innerHTML=u}function h(o){const s=`https://pixabay.com/api/?key=43705346-f08330685c72fc18a8a8b3aad&q=${o}&image_type=photo&orientation=horizontal&safesearch=true`;fetch(s).then(i=>{if(!i.ok)throw new Error("Network response was not ok");return i.json()}).then(i=>{i.hits.length===0?c():d(i.hits)}).catch(y).finally(()=>{l.reset()})}function c(){f.error({message:"Sorry, there are no images matching your search query. Please try again!"})}function d(o){g(),o.forEach(r=>{p(r),new m(".photo-list a",{}).refresh()})}function y(o){alert(o)}function g(){a.innerHTML=""}const a=document.querySelector(".img-container"),l=document.querySelector(".search-form");document.querySelector(".loader");const b=document.querySelector(".search-input");l.addEventListener("submit",L);function L(o){o.preventDefault();const r=b.value.trim();r!==""&&h(r),c()}
 //# sourceMappingURL=commonHelpers.js.map
